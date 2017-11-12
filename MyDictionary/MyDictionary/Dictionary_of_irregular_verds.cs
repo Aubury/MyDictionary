d81fe90ad2746670b92057ebf6a04dee7f19e30c
Tash <aubury@ukr.net>
2017-11-12 21:35:56 +0200

    Start

---
+++ MyDictionary/MyDictionary/Dictionary_of_irregular_verds.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class Dictionary_of_irregular_verds
+    {
+    }
+}
